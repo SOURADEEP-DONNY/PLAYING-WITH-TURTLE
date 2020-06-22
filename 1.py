760ca490a276b4bc6cbf0561429084f612763583
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-22 17:23:17 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+#DESIGNING THE FIRST LETTER OF MY NAME.
+
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("lightgreen")
+t1=turtle.Turtle()
+
+t1.shape("turtle")
+t1.forward(50)
+t1.left(180)
+t1.forward(50)
+t1.left(90)
+t1.forward(50)
+t1.left(90)
+t1.forward(50)
+t1.right(90)
+t1.forward(50)
+t1.right(90)
+t1.forward(50)
+
+
