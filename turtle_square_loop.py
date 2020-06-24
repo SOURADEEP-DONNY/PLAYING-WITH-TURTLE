b9739d3c8cb9c7272793fbf6127e9eb7919adf0e
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-24 12:30:15 +0530

    Add files via upload

--- turtle_square_loop.py
+++ turtle_square_loop.py
@@ -5,10 +5,12 @@ wn=turtle.Screen()
 wn.bgcolor("hotpink")
 t=turtle.Turtle()
 t.shape("turtle")
+t.pensize(5)
+t.color("blue")
 
 for i in [1,2,3,4]:
+    t.stamp()
     t.forward(100)
     t.right(90)
-
 
 wn.exitonclick()
